70a8ae533d53d52dd45c71629e7babc81bcadb58
ssamko <ssamko@gdflab.com>
2020-10-29 17:47:47 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by ssamko on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
