ff5f486fd5c30a25bb3f924ad057a64f33aa7315
ssamko <teacher.ssamko@gmail.com>
2020-12-19 01:37:36 +0900

    first swiftUI

---
+++ 1_stackView/ToDo/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  ToDo
+//
+//  Created by lee eunseop on 2020/12/19.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("tree")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth:4))
+            .shadow(radius: 7)
+        
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
