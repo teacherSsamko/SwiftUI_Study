13daf3b5959f5e8b6b0bcb084ed4734730d2173c
ssamko <teacher.ssamko@gmail.com>
2020-12-21 19:28:42 +0900

    Dynamic Data

---
+++ ToDo/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by ssamko on 2020/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        LandmarkList()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- ToDo/Views/LandmarkDetail.swift
+++ ToDo/Views/LandmarkDetail.swift
@@ -1,49 +1,51 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  ToDo
 //
-//  Created by ssamko on 2020/10/29.
+//  Created by lee eunseop on 2020/12/21.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
+    var landmark: Landmark
     
     var body: some View {
-        VStack{
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea( edges: .top)
                 .frame(height:300)
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y:-130)
                 .padding(.bottom,-130)
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                         .foregroundColor(.black)
                     .font(.title)
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     
                     Spacer()
-                    Text("Califonia")
+                    Text(landmark.state)
                         
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
                 Divider()
-                Text("About Turtle Rock")
+                Text("About \(landmark.name)")
                     .font(.title2)
-                Text("Descriptive text goes here")
+                Text(landmark.description)
             }
             .padding()
-            Spacer()
+            
         }
-        
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
