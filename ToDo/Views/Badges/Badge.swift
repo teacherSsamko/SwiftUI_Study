a86b6fcdee63dd8d974a1e28e766aa944eca22e4
ssamko <teacher.ssamko@gmail.com>
2020-12-25 00:21:33 +0900

    Drawing paths and shapes

---
+++ ToDo/Views/Badges/Badge.swift
@@ -0,0 +1,39 @@
+//
+//  Badge.swift
+//  ToDo
+//
+//  Created by lee eunseop on 2020/12/25.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(angle: .degrees(Double(i) / Double(Badge.rotationCount) * 360))
+                .opacity(0.5)
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2, y: (3.0/4.0) * geometry.size.height)
+                
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
