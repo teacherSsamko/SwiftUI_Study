f5354b1eac8fb019ccb23cb32178c4a2e1bb8b42
ssamko <teacher.ssamko@gmail.com>
2020-12-29 00:26:33 +0900

    UI Contorls

---
+++ ToDo/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  ToDo
+//
+//  Created by lee eunseop on 2020/12/28.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var preferNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username:"g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌹"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+    }
+    
+}
